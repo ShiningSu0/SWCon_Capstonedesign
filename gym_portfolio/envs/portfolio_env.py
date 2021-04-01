7fabe7940548c68cd0bcd228ff28cc972eb2f864
Suyeong Kim 김수영 <44190559+ShiningSu0@users.noreply.github.com>
2021-04-01 20:45:23 +0900

    UPDATE portfolio_env
    환경 구성 중!

--- gym_portfolio/envs/portfolio_env.py
+++ gym_portfolio/envs/portfolio_env.py
@@ -1,15 +1,57 @@
-import os, subprocess, time, signal
 import gym
-from gym import error, spaces
-from gym import utils
+from gym import error, spaces, utils
 from gym.utils import seeding
+import numpy as np
+import pandas as pd
 
-try:
-    import hfo_py
-except ImportError as e:
-    raise error.DependencyNotInstalled("{}. (HINT: you can install HFO dependencies with 'pip install gym[PortfolioEnv].)'".format(e))
+def flip(edge, np_random):
+    return 1 if np_random.uniform() < edge else -1
 
-import logging
-logger = logging.getLogger(__name__)
 
-class PortfolioEnv(gym.Env, utils.EzPickle):
+class PortfolioEnv(gym.Env):
+  metadata = {'render.modes': ['human']}
+
+  def __init__(self,  edge=0.6, max_wealth=250.0, max_rounds=300):
+      self.action_space = spaces.Discrete(int(max_wealth * 100))  # betting in penny
+      #할 수 있는 경우의 수가 Discrete( ) 안에 들어감!
+      # increments
+      self.observation_space = spaces.Tuple((
+          spaces.Box(0, max_wealth, [1], dtype=np.float32),  # (w,b)
+          spaces.Discrete(max_rounds + 1))) # https://stackoverflow.com/questions/57583185/what-does-spaces-discrete-mean-in-openai-gym
+      self.reward_range = (0, max_wealth)
+      self.edge = edge
+      self.wealth = 100000
+      self.initial_wealth = 100000 #Starts at $100,000
+      self.max_rounds = max_rounds
+      self.max_wealth = max_wealth
+      self.np_random = None
+      self.rounds = None
+      self.seed()
+      self.reset()
+
+  def seed(self, seed=None):
+      self.np_random, seed = seeding.np_random(seed)
+      return [seed]
+
+  def step(self, action):
+      bet_in_dollars = min(action / 100.0, self.wealth)  # action = desired bet in pennies
+      self.rounds -= 1
+
+      coinflip = flip(self.edge, self.np_random)
+      self.wealth = min(self.max_wealth, self.wealth + coinflip * bet_in_dollars)
+
+      done = self.wealth < 0.01 or self.wealth == self.max_wealth or not self.rounds
+      reward = self.wealth if done else 0.0
+
+      return self._get_obs(), reward, done, {}
+
+  def _get_obs(self):
+      return np.array([self.wealth]), self.rounds
+
+  def reset(self):
+      self.rounds = self.max_rounds
+      self.wealth = self.initial_wealth
+      return self._get_obs()
+
+  def render(self, mode='human'):
+      print("Current wealth: ", self.wealth, "; Rounds left: ", self.rounds)
