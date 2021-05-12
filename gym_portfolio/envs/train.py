938ddeba0303925a7cf54f076af71d71abe4d235
Suyeong Kim 김수영 <44190559+ShiningSu0@users.noreply.github.com>
2021-05-13 00:53:49 +0900

    UPDATE visualize.py

--- gym_portfolio/envs/train.py
+++ gym_portfolio/envs/train.py
@@ -5,19 +5,11 @@ import gym
 import numpy as np
 from gym_portfolio.envs.portfolio_env import PortfolioEnv
 import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib import interactive
 import math
 import random
 import seaborn as sns
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 from collections import namedtuple
 from itertools import count
-from PIL import Image
-
 from torch.autograd import Variable
 import torch
 import torch.nn as nn
